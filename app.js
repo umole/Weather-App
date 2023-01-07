a6882997c8d6ec4ac3c99bb7653f16ebd254b2fb
Umole Andrew <39883080+umole@users.noreply.github.com>
2023-01-07 13:33:01 +0100

    Responsive app allowing users to input their city

--- app.js
+++ app.js
@@ -2,34 +2,44 @@ const express = require("express");
 const { write } = require("fs");
 const https = require('https');
 const axios = require('axios');
-
-const CITY_ID = 2234402; // Lagos, Nigeria
-const API_KEY = "6ca92c89e79c61f4bed2675936ad2bcb";
-const CITY_NAME = "Abeokuta";
-
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 
 app.get('/', (req, res) => {
-   // res.send("Hello, you wanna check the weather update?");
+   res.sendFile(__dirname + "/index.html");
+    
+});
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${API_KEY}&units=metric`;
+app.post('/', (req, res) => {
+    const CITY_ID = 2234402; // Lagos, Nigeria
+    const API_KEY = "6ca92c89e79c61f4bed2675936ad2bcb";
+    const CITY_NAME = req.body.city;
+    const UNIT = "metric";
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${API_KEY}&units=${UNIT}`;
 
     axios.get(url)
         .then((response) => {
-            const weatherData = response.data.weather[0].description;
+            const weatherData = (response.data.main.temp).toFixed(1) //.weather[0].description;
             const rawWeatherDataIcon = response.data.weather[0].icon;
-            res.write(`<h1>The weather description is: ${weatherData}</h1>`);
             const weatherImage = `http://openweathermap.org/img/wn/${rawWeatherDataIcon}@2x.png`
-            res.write("<img src=" + weatherImage + ">");
-            res.send();
+            res.send(`
+           <h1>The weather temperature in ${CITY_NAME} is: ${weatherData} degree celcius</h1>
+           <img src="${weatherImage}">
+           `);
     })
         .catch((error) => {
             console.error(error);
             res.status(500).send({error: 'Something went wrong'});
     });
-});
+
+})
+
 
 app.listen(3000, () => {
     console.log("Server is listening on PORT: 3000");

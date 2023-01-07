584eb58a48e469486ca2ff1724d39fa4396ed52a
Umole Andrew <39883080+umole@users.noreply.github.com>
2023-01-07 11:39:10 +0100

    weather image

--- app.js
+++ app.js
@@ -14,12 +14,16 @@ const app = express();
 app.get('/', (req, res) => {
    // res.send("Hello, you wanna check the weather update?");
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${API_KEY}&unit=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${API_KEY}&units=metric`;
 
     axios.get(url)
         .then((response) => {
             const weatherData = response.data.weather[0].description;
-            res.send(`The weather description is: ${weatherData}`);
+            const rawWeatherDataIcon = response.data.weather[0].icon;
+            res.write(`<h1>The weather description is: ${weatherData}</h1>`);
+            const weatherImage = `http://openweathermap.org/img/wn/${rawWeatherDataIcon}@2x.png`
+            res.write("<img src=" + weatherImage + ">");
+            res.send();
     })
         .catch((error) => {
             console.error(error);
